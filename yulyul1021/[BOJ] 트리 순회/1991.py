b2668f73f14c93751b4ea022b91a07cdf6035075
yulyul1021 <108569281+yulyul1021@users.noreply.github.com>
2024-03-28 19:56:19 +0900

    [BOJ] 트리 순회 / S1

---
+++ yulyul1021/[BOJ] 트리 순회/1991.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+
+n = int(sys.stdin.readline().strip())
+tree = {}
+
+for i in range(n):
+    root, left, right = sys.stdin.readline().strip().split()
+    tree[root] = [left, right]
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
